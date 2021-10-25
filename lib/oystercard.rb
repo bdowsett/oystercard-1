a626d71789c677436ab1ff55ced027a9bf15b64d
Ben Dowsett <bendowsett@gmail.com>
2021-10-25 17:02:16 +0100

    step 6: added test for maximum balance and code to pass

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class OysterCard
    attr_reader :balance
-
+   
+   MAXIMUM_BALANCE = 90
+   
    def initialize
     @balance = 0
    end
 
    def top_up(money)
+    fail "your balance is already #{MAXIMUM_BALANCE}" if money + @balance > MAXIMUM_BALANCE
     @balance += money
    end 
 end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,6 +12,11 @@ describe OysterCard do
       end
       it 'can top up balance' do
         expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-    end
-end  
+      end 
+      it "balance cannot exceed £90" do 
+        maximum_balance = OysterCard::MAXIMUM_BALANCE
+        subject.top_up(maximum_balance)
+        expect { subject.top_up 1}.to raise_error "your balance is already #{maximum_balance}"
+      end 
+    end  
 end
